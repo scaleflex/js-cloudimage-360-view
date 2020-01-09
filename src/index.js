1e8feb4812a7971f28cf46ce5b34eb2e3051ecc1
Rumen Krastev Shishkov <rumen@scaleflex.com>
2020-01-09 18:01:27 +0200

    Fixed ie9 controls issues

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+import 'classlist.js';
+
 import { Viewer } from "./viewer";
 import './stylesheets/ci360.scss';
 
