166e4aab2c62cd90e4fd227eae19b32214d2d083
Amr Wagdy <amrw.js@gmail.com>
2021-06-02 20:12:31 +0200

    back to default

--- src/ci360.service.js
+++ src/ci360.service.js
@@ -491,7 +491,7 @@ class CI360Viewer {
 
   openFullScreenModal() {
     const fullScreenModal = document.createElement('div');
-    window.document.body.style.overflow = 'hidden';
+
     setFullScreenModalStyles(fullScreenModal);
 
     const fullScreenContainer = this.container.cloneNode();
@@ -510,8 +510,6 @@ class CI360Viewer {
 
   closeFullScreenModal() {
     document.body.removeChild(this.container.parentNode);
-    window.document.body.style.overflow = 'visible';
-
   }
 
   add360ViewCircleIcon() {
